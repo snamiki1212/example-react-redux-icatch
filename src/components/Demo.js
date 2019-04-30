8c57eb7675680e82d2fe02753243b3fef2d4e343
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 20:54:57 +0700

    Update design

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -6,6 +6,10 @@ const Demo = ({ stringColor, backgroundColor }) => {
     <div
       id={GENERATER_INPUT_ID}
       style={{
+        height: '200px',
+        width: '200px',
+        borderRadius: '10px',
+        marginTop: '100px',
         backgroundColor: backgroundColor,
         color: stringColor
       }}
