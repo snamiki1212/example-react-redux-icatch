f550d6d79246acc735e5b68cd7fc4ac2cc67fc46
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 18:28:52 +0700

    Remove not using

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DemoContainer from './containers/DemoContainer';
 import Controller from './containers/Controller'
@@ -11,23 +10,6 @@ function App() {
       <DemoContainer />
       <Controller />
       <Output />
-      <header className="App-header">
-        <div id='my-node'>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
-      </header>
     </div>
   );
 }
