fd196ac7f86ece9abbbc89b756a2b125d3d78d2c
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 18:15:27 +0700

    Change picker

--- src/containers/Controller.js
+++ src/containers/Controller.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import creater from '../components/creater';
-import { SketchPicker } from 'react-color';
+import { TwitterPicker } from 'react-color';
 import { updateBackgroundColor } from '../actions';
-
-// const handleChangeComplete = (color) => {
-//   console.log(color.hex);
-//   dispatch(updateBackgroundColor(color.hex));
-//   return {
-//     background: color.hex
-//   };
-// }
 
 const Controller = ({ updateBackgroundColor, backgroundColor }) => {
   return(
     <div>
       Controller
-      <SketchPicker
+      <TwitterPicker
         color={backgroundColor}
         onChangeComplete={(color) => updateBackgroundColor(color.hex)}
       />
