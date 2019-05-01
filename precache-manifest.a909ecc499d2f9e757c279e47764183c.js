008a8e78b92fed33747ea87b5760547d21b23957
snamiki1212 <snamiki1212@gmail.com>
2019-05-01 19:22:35 +0700

    Updates

---
+++ precache-manifest.a909ecc499d2f9e757c279e47764183c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308e620b4358352bf2d6f30ae5a8cb18",
+    "url": "/icatch/index.html"
+  },
+  {
+    "revision": "b1f0c245ee99c7c42d46",
+    "url": "/icatch/static/css/main.aa533529.chunk.css"
+  },
+  {
+    "revision": "78f4f6e4ac1911d3e955",
+    "url": "/icatch/static/js/2.ee8e742c.chunk.js"
+  },
+  {
+    "revision": "b1f0c245ee99c7c42d46",
+    "url": "/icatch/static/js/main.d1ff492e.chunk.js"
+  },
+  {
+    "revision": "8ff8ac937adde071390e",
+    "url": "/icatch/static/js/runtime~main.1da1156b.js"
+  }
+]);
